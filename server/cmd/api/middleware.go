b31da003a3cae559694a1c3932f453ef5cc0eeeb
araaavind <aravindmathradan2@gmail.com>
2025-03-16 21:54:55 +0530

    implemented panic recovery

---
+++ server/cmd/api/middleware.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event of a panic
+		// as Go unwinds the stack).
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				// The value returned by recover() has the type any, so we use
+				// fmt.Errorf() to normalize it into an error
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
